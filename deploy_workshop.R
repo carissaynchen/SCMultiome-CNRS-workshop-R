7f1bc1ca2987a8c3bc7c725074c193ab2bd0b659
Carissa Chen <carissachen97@gmail.com>
2024-05-13 02:03:01 +1000

    edit machineType

--- deploy_workshop.R
+++ deploy_workshop.R
@@ -1,7 +1,7 @@
 Sys.setenv(GCE_AUTH_FILE = '/Users/cchen/Dropbox (Sydney Uni)/workshop-CNRS/sunny-might-421907-m4-04c5b2e4865b.json')
 library(googleComputeEngineR)
 
-gce_global_project("test_project")
+gce_global_project("test-project")
 gce_global_zone("europe-west1-b")
 
 ## see gce_list_machinetype() for options of predefined_type
@@ -16,11 +16,11 @@ vm1 <- gce_vm(template = "rstudio",
               user = "rstudio",
               password = "test")
 
-vm2 <- gce_vm(template = "rstudio",
-             name = "scmultiomecnrs2",
-             disk_size_gb = 10,
-             # predefined_type = "n2-standard-32",
-             predefined_type = "e2-micro",
-             dynamic_image = tag,
-             user = "rstudio",
-             password = "test")
+# vm2 <- gce_vm(template = "rstudio",
+#              name = "scmultiomecnrs2",
+#              disk_size_gb = 10,
+#              # predefined_type = "n2-standard-32",
+#              predefined_type = "e2-micro",
+#              dynamic_image = tag,
+#              user = "rstudio",
+#              password = "test")
