c8e24088ad5056a8a91b241f969fd6fcfbee4f80
Carissa Chen <carissachen97@gmail.com>
2024-05-14 16:35:51 +1000

    change project

--- deploy_workshop.R
+++ deploy_workshop.R
@@ -1,11 +1,11 @@
 Sys.setenv(GCE_AUTH_FILE = '/Users/cchen/Dropbox (Sydney Uni)/workshop-CNRS/sunny-might-421907-m4-04c5b2e4865b.json')
 library(googleComputeEngineR)
 
-gce_global_project("test-project")
+gce_global_project("sunny-might-421907-m4")
 gce_global_zone("europe-west1-b")
 
 ## see gce_list_machinetype() for options of predefined_type
-tag = "gcr.io/sunny-might-421907-m4/scmultiomecnrsR:alpha"
+tag = "gcr.io/sunny-might-421907-m4/scmultiomecnrsr:alpha"
 
 vm1 <- gce_vm(template = "rstudio",
               name = "scmultiomecnrs1",
